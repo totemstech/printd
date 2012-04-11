6934691c6d0af02eda96fcacda679063abe54fa8
Mohamed Ahmednah <mohamed@teleportd.com>
2012-04-11 10:56:53 +0200

    removed json

---
+++ printd/PageController.h
@@ -0,0 +1,13 @@
+//
+//  PageController.h
+//  printd
+//
+//  Created by Mohamed Ahmednah on 4/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PageController : NSObject
+
+@end
---
+++ printd/PageController.m
@@ -0,0 +1,13 @@
+//
+//  PageController.m
+//  printd
+//
+//  Created by Mohamed Ahmednah on 4/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PageController.h"
+
+@implementation PageController
+
+@end
